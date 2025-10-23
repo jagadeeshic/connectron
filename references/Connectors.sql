b82331af6a5d8ca5792c20f83ccb9e4965819a52
Metta Jagadeesh <41304408+jagadeeshic@users.noreply.github.com>
2025-10-23 19:22:15 +0530

    Add files via upload
    Added via Desktop

---
+++ references/Connectors.sql
@@ -0,0 +1,265 @@
+Azure:
+    Azure Blob Storage
+    Azure Data Explorer
+    Azure Data Lake Storage Gen2
+    Azure File Storage
+    Azure Table Storage
+
+Data Warehouse:
+    Amazon RedShift
+    Azure Databricks Deltalake
+    Azure Fabirc Warehouse
+    Google BigQuery
+    Greenplum
+    Apache Hive
+    IBM Informix
+    Maria DB
+    Snowflake
+    Spark
+    Syabse
+    Terdata
+    Vertica
+    Oracle
+    Netezza
+
+File:
+    Amazon S3
+    Azure Blob 
+    Google Cloud Storage
+    FTP
+    File System
+    HDFS
+    SFTP
+    Oracle S3
+
+Database:
+    Amazon RDS for Oracle
+    Amazon RDS for SQL Server
+    Apache Impala
+    IBM DB2
+    Azure Database MySQL
+    Azure Database PostgreSQL
+    Azure SQL Database
+    Microsoft Access
+    PostgreSQL
+    Presto
+    SQL Server
+
+NoSQL:
+    Azure Cosmos for Mongo DB
+    Azure Cosmos for NoSQL
+    Apache Cassandra
+    MongoDB
+    MongoDB Atlas
+
+ERP:
+    SAP BW Open Hub
+    SAP BW
+    SAP HANA
+    SAP Table
+
+SaaS:
+    Google Ads
+    Dataverse
+    Dynamics 365
+    Dynamics Ax
+    Dynamics CRM
+    HubSpot
+    Jira
+    Microsoft 365
+    QuickBooks
+    SAP Cloud for Customer
+    SAP ECCC
+    Salesforce Service Cloud v2
+    Salesforce v2
+    ServiceNow 
+    ServiceNow v2
+    Sharepoint 
+    Shopify
+    Sqaure
+    Xero
+    Workday Reports
+    Adjust
+    Adobe Analytics
+    OneDrive
+    Google Drive 
+    Github
+    Adobe Analytics DataFeed
+    ADP Workforce Now 
+    AdRoll
+    Airtable
+    Amplitude
+    Anaplan
+    Apache Kafka
+    Apple App Store 
+    Apple Search Ads
+    AppsFlyer 
+    Asana 
+    Aurora MySQL
+    Aurora PostgreSQL
+    Aws CloudTail
+    AWS Inventory
+    AWS Lambda
+    AWS MSK
+    Azure Event Hubs 
+    Azure Function
+    Box 
+    Braintree
+    Braintree Sandbox
+    Branch
+    Braze
+    AWS CloudFront
+    Confluent Cloud
+    Consumer DropBox
+    Coupa
+    Criteo
+    Delighted
+    Dynamics 365 FO 
+    Eloqua
+    Email
+    Facebook Ads
+    Facebook Pages
+    FinancialForce
+    Fivetran Log
+    Azure Data Factory Log
+    FreshDesk
+    Front 
+    Google Ads Manager
+    Google Analytics
+    Google Analytics 360
+    Google Campaign Manager
+    Google Cloud Function
+    Google Display & video 360
+    Google Play
+    Google Search Console
+    Google Sheets
+    GreenHouse
+    Heap
+    Height
+    Help Scout
+    Intagram Business
+    Intercom
+    Iterable
+    Kinesis
+    Klaviyo
+    Kustomer
+    Lever
+    Lightspeed Retail
+    Linkedin Ad Analytics
+    Linkedin Company Pages
+    Magento
+    Mailchimp
+    Mandrill
+    Marin Software
+    Marketo
+    MavenLink
+    Microsoft Advertising 
+    Mixpanel
+    Netsuite
+    New Relic
+    Optimizely
+    Oracle DB
+    Outbrain
+    Outreach
+    Paradot
+    Paypal
+    Pendo
+    Pintrest Ads
+    Pipedrive
+    Qualtrics
+    ReCharge
+    Recurly
+    Sage Intact
+    Sailthru
+    SAP Concur
+    Segement
+    SendGrid
+    Snapchat Ads
+    Snowplow 
+    Splunk
+    Stripe
+    SurveryMonkey
+    Taboola
+    The Trade Desk 
+    Tiktok Ads 
+    Twilio
+    Twitter
+    Twitter Ads 
+    Typeform
+    Uservoice
+    verizon Media
+    Wordpress
+    Youtube Analytics
+    Zendesk 
+    Zoho CRM 
+    Zuora
+
+AirByte:
+    GitLab
+    Notion
+    Slack
+    100ms
+    7Shifts
+    Awin
+    Adyen
+    Aha 
+    MotherDuck
+    Dremio
+    Elastic
+    Jotform
+    Gong
+    TicketMaster
+    Brevo
+    BugCrowd
+    ChargeBee
+    CircleCI
+    ClickUp
+    ConcordNow
+    Datadog
+    Dixa
+    Evenbrite
+    Float
+    FireHydrant
+    Infobip
+    LDAP
+    Launch Darkly
+    Mailgun
+    MindSphere
+    Ongoing Exchange Rates
+    Orbit
+    Ortto
+    PagerDuty
+    SmartSheet
+    Toggl
+    Yelp
+    Snyk 
+    SeatGeek
+    Salesloft
+    Rootly
+    Persona
+    Pingdom
+    Pivotal Tracker
+    Product Board
+    Sage Intact
+    Ring Central
+    
+
+AI:
+    OpenAI
+    Gemini
+    Anthropic
+    Copilot
+    Claude
+    
+
+Apache Airflow:
+    Exasol
+    Dingding
+    Singularity
+    Redis 
+    Discord
+    Telegram
+    Celery
+    Qubole
+    
+
---
+++ references/PhaseOne.sql
@@ -0,0 +1,26 @@
+Phase 1 Connectors:
+
+    Source:
+        Microsoft Dataverse
+        Microsoft OneDrive
+
+        Social Media:
+            Instagram
+            Facebook
+            Google Ads
+            Apple Ads
+            Snapchat Ads
+            Twitter Ads
+
+    Phase1 Sink:
+        Azure Blob
+        AWS S3
+
+
+    @All Sink will be Raw Json/xml/csv etc.
+    Sink:
+        Azure Blob 
+        AWS S3
+        Google Cloud Storage
+        Oracle S3
+        CloudFlare R3
